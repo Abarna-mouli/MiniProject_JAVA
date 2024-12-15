f28f115f1795c48e6fd15ae3cd9fc25fa48408b3
Abarna-mouli <133858723+Abarna-mouli@users.noreply.github.com>
2024-12-15 19:03:56 +0530

    Update MiniProject.java

--- MiniProject.java
+++ MiniProject.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
-class NewChannel {
-    String programName;
-    String anchor;
-    int month;
-    double trpRating;
-    double business;
+// Abstraction and Inheritance
+abstract class Channel {
+    private String programName;
+    private String anchor;
+    private int month;
+    private double trpRating;
+    private double business;
 
-    public NewChannel(String programName, String anchor, int month, double trpRating, double business) {
+    public Channel(String programName, String anchor, int month, double trpRating, double business) {
         this.programName = programName;
         this.anchor = anchor;
         this.month = month;
@@ -15,22 +16,75 @@ class NewChannel {
         this.business = business;
     }
 
+    // Encapsulation
+    public String getProgramName() {
+        return programName;
+    }
+
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+
+    public String getAnchor() {
+        return anchor;
+    }
+
+    public void setAnchor(String anchor) {
+        this.anchor = anchor;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public double getTrpRating() {
+        return trpRating;
+    }
+
+    public void setTrpRating(double trpRating) {
+        this.trpRating = trpRating;
+    }
+
+    public double getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(double business) {
+        this.business = business;
+    }
+
+    // Abstract method
+    public abstract void display();
+}
+
+class NewChannel extends Channel {
+
+    public NewChannel(String programName, String anchor, int month, double trpRating, double business) {
+        super(programName, anchor, month, trpRating, business);
+    }
+
+    // Polymorphism (Method Overriding)
+    @Override
     public void display() {
-        System.out.println("\nProgram name: " + programName +
-                "\nAnchor: " + anchor +
-                "\nMonth: " + month +
-                "\nTRP rating: " + trpRating +
-                "\nBusiness profit (in millions): " + business);
+        System.out.println("\nProgram name: " + getProgramName() +
+                "\nAnchor: " + getAnchor() +
+                "\nMonth: " + getMonth() +
+                "\nTRP rating: " + getTrpRating() +
+                "\nBusiness profit (in millions): " + getBusiness());
     }
 }
 
-public class MiniProject {
+public class Oops {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of programs:");
         int n = sc.nextInt();
-        NewChannel[] programs = new NewChannel[n];
+        Channel[] programs = new Channel[n];
 
         for (int i = 0; i < n; i++) {
             System.out.println("\n----------Enter details for program " + (i + 1) + "----------- ");
@@ -53,11 +107,11 @@ public class MiniProject {
         findBusyAnchor(programs);
     }
 
-    private static void findMaxCollectionMonth(NewChannel[] programs) {
+    private static void findMaxCollectionMonth(Channel[] programs) {
         double[] monthlyTotal = new double[12];
 
-        for (NewChannel program : programs) {
-            monthlyTotal[program.month - 1] += program.business;
+        for (Channel program : programs) {
+            monthlyTotal[program.getMonth() - 1] += program.getBusiness();
         }
 
         double maxProfit = 0;
@@ -70,32 +124,35 @@ public class MiniProject {
             }
         }
 
-        System.out.println("\nMonth with maximum collection: " + maxMonth + " (January optional)");
+        System.out.println("\nMonth with maximum collection: " + maxMonth );
+	System.out.println("---------------------------------------------------------------");
+
         System.out.println("Programs with their profits for the month:");
         double totalProfit = 0;
-        for (NewChannel program : programs) {
-            if (program.month == maxMonth) {
-                System.out.println("Program name: " + program.programName + "\tProfit: " + program.business + " million");
-                totalProfit += program.business;
+        for (Channel program : programs) {
+            if (program.getMonth() == maxMonth) {
+                System.out.println("Program name: " + program.getProgramName() +"\t"+ "\tProfit: " + program.getBusiness() + " million");
+                totalProfit += program.getBusiness();
             }
         }
+	System.out.println("---------------------------------------------------------------");
         System.out.println("Total profit for the month: " + totalProfit + " million");
     }
 
-    private static void findBusyAnchor(NewChannel[] programs) {
+    private static void findBusyAnchor(Channel[] programs) {
         String busiestAnchor = "";
         int maxCount = 0;
 
-        for (NewChannel program : programs) {
+        for (Channel program : programs) {
             int count = 1;
-            for (NewChannel otherProgram : programs) {
-                if (otherProgram != program && otherProgram.anchor.equals(program.anchor)) {
+            for (Channel otherProgram : programs) {
+                if (otherProgram != program && otherProgram.getAnchor().equals(program.getAnchor())) {
                     count++;
                 }
             }
             if (count > maxCount) {
                 maxCount = count;
-                busiestAnchor = program.anchor;
+                busiestAnchor = program.getAnchor();
             }
         }
 
